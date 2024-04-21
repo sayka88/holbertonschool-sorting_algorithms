e520d9dd77040fee89cd9612d8453d06a91d7286
sayka88 <sayka.alekperova@gmail.com>
2024-04-21 06:20:50 +0000

    1-insertion_sort_list.c modified

--- test/1-insertion_sort_list.c
+++ test/1-insertion_sort_list.c
@@ -11,7 +11,8 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
+	listint_t *current;
+	current = (*list)->next;
 	listint_t *prev;
 
 	while (current != NULL)
