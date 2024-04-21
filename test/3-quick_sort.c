916a0ec10f95050d6e89a119f5023525e51717fd
sayka88 <sayka.alekperova@gmail.com>
2024-04-21 06:52:43 +0000

    modif test folder

---
+++ test/3-quick_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int partition(int *array, int low, int high) {
+	int pivot = array[high];
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++) {
+		if (array[j] < pivot) {
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+
+void quick_sort_helper(int *array, int low, int high) {
+	if (low < high) {
+		int pi = partition(array, low, high);
+
+		quick_sort_helper(array, low, pi - 1);
+		quick_sort_helper(array, pi + 1, high);
+	}
+}
+
+void quick_sort(int *array, size_t size) {
+	quick_sort_helper(array, 0, size - 1);
+}
